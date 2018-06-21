07bcb42ca509a8c169545f93752c69f69a93bfd5
Nommington <rymckenz@gmail.com>
2018-06-20 20:04:12 -0400

    populated scores/comparison logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,62 +2,194 @@ var countryArr = [
     {
         "name": "France",
         "flag": "flag",
-        "scores": []
+        "scores": [
+            2,
+            2,
+            4,
+            3,
+            4,
+            1,
+            4,
+            5,
+            1,
+            5
+        ]
     },
     {
         "name": "Great Britain",
         "flag": "flag",
-        "scores": []
+        "scores": [
+            2,
+            3,
+            3,
+            5,
+            3,
+            1,
+            5,
+            2,
+            1,
+            5
+        ]
     },
     {
         "name": "Russia",
         "flag": "flag",
-        "scores": []
+        "scores": [
+            5,
+            5,
+            4,
+            4,
+            5,
+            5,
+            4,
+            4,
+            1,
+            4
+        ]
     },
     {
         "name": "Germany",
         "flag": "flag",
-        "scores": []
+        "scores": [
+            2,
+            3,
+            5,
+            2,
+            5,
+            3,
+            5,
+            4,
+            1,
+            2
+        ]
     },
     {
         "name": "Austria-Hungary",
         "flag": "flag",
-        "scores": []
+        "scores": [
+            3,
+            3,
+            3,
+            1,
+            3,
+            2,
+            4,
+            4,
+            1,
+            1
+        ]
     },
     {
         "name": "Ottoman Empire",
         "flag": "flag",
-        "scores": []
+        "scores": [
+            3,
+            2,
+            2,
+            4,
+            2,
+            5,
+            2,
+            2,
+            2,
+            5
+        ]
     },
     {
         "name": "Bulgaria",
         "flag": "flag",
-        "scores": []
+        "scores": [
+            1,
+            1,
+            1,
+            2,
+            1,
+            2,
+            1,
+            2,
+            4,
+            5
+        ]
     },
     {
         "name": "Serbia",
         "flag": "flag",
-        "scores": []
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            1,
+            3,
+            1,
+            5,
+            1,
+            4
+        ]
     },
     {
-        "name": "Japan",
+        "name": "Belgium",
         "flag": "flag",
-        "scores": []
+        "scores": [
+            2,
+            1,
+            1,
+            2,
+            1,
+            1,
+            2,
+            5,
+            1,
+            5
+        ]
     },
     {
         "name": "Italy",
         "flag": "flag",
-        "scores": []
+        "scores": [
+            3,
+            2,
+            3,
+            4,
+            2,
+            2,
+            3,
+            2,
+            3,
+            5
+        ]
     },
     {
         "name": "Romania",
         "flag": "flag",
-        "scores": []
+        "scores": [
+            2,
+            1,
+            1,
+            2,
+            2,
+            2,
+            2,
+            3,
+            4,
+            5
+        ]
     },
     {
         "name": "United States",
         "flag": "flag",
-        "scores": []
+        "scores": [
+            4,
+            3,
+            2,
+            3,
+            1,
+            1,
+            4,
+            1,
+            5,
+            5
+        ]
     },
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,37 @@
 var countriesData = require("../data/friends.js");
 
+
+
 module.exports = function (app) {
     app.get("/api/countries", function (req, res) {
         res.json(countriesData);
+    });
+
+    app.post("/api/countries", function(req, res){
+        var bffCountry = {
+            name: "",
+            flag: "",
+            diff: 500
+        };
+
+        var yourCountryScore = req.body.scores;
+        var scoreDiff = 0;
+
+        for (i=0; i<countriesData.length; i++) {
+            var country = countriesData[i];
+            scoreDiff = 0;
+
+            for (j=0; j<country.scores.length; j++){
+                scoreDiff += Math.abs(parseInt(yourCountryScore[j]) - parseInt(country.scores[j]));
+            }
+            if (scoreDiff >= bffCountry.diff) {
+                bffCountry.name = country.name;
+                bffCountry.flag = country.flag;
+                bffCountry.diff = scoreDiff;
+            }
+        }
+        countriesData.push(req.body);
+        res.json(bffCountry);
     });
 };
 
